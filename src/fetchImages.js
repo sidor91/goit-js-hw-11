c0dd737344e353b0dcec3806db257d03d64774bc
Serhii Sydorenko <sidorchik91@gmail.com>
2023-02-04 23:36:11 +0200

    created class for fetching images

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,14 +1,31 @@
-const axios = require('axios').default;
+const axios = require('axios').default; 
 
 
-export async function fetchImages(query) {
-  const searchParams = new URLSearchParams({
-    key: '33349547-44f128e159fc9ba4be7374396',
-    q: `${query}`,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-    const fetchUrl = await axios.get(`https://pixabay.com/api/?${searchParams}`);
+export default class FetchImagesFromPixabay {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+  }
+  async fetchImages() {
+    const searchParams = new URLSearchParams({
+      key: '33349547-44f128e159fc9ba4be7374396',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: 40,
+    });
+    const fetchUrl = await axios.get(
+      `https://pixabay.com/api/?q=${this.searchQuery}&page=${this.page}&${searchParams}`
+    );
+      this.page += 1;
     return fetchUrl.data;
+  }
+    
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,79 @@
 import { fetchImages } from './fetchImages';
 import Notiflix from 'notiflix';
-const axios = require('axios').default;
+import FetchImagesFromPixabay from './fetchImages';
+const fetchImagesFromPixabay = new FetchImagesFromPixabay();
 
+
+const axios = require('axios').default;
 
 const searchForm = document.querySelector('.search-form');
 const galleryOfImages = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
 
 searchForm.addEventListener('submit', onFormSubmit);
-
-
-async function onFormSubmit(e) {
-  e.preventDefault();
-    galleryOfImages.innerHTML = '';
-    try {
-         const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-         const images = await fetchImages(searchQuery);
-         console.log(images);
-         if (images.total === 0) {
-           Notiflix.Notify.failure(
-             'Sorry, there are no images matching your search query. Please try again.'
-           );
-         }
-         renderImages(images.hits);
-    } catch (error) {
-      Notiflix.Notify.failure(`${error}`);
-    }
-    searchForm.reset();
-}
+loadMoreButton.addEventListener('click', onLoadMoreButtonClick);
 
 
 function renderImages(images) {
-  const imagesMarkup = images.map(image => {
-      return `<div class="photo-card"><img src=${image.webformatURL} alt=${image.tag} loading="lazy"/><div class="info"><p class="info-item"><b>Likes: ${image.likes}</b></p><p class="info-item"><b>Views: ${image.views}</b></p><p class="info-item"><b>Comments: ${image.comments}</b></p><p class="info-item"><b>Downloads: ${image.downloads}</b></p></div></div>`;
+  const imagesMarkup = images
+    .map(image => {
+      return `<div class="photo-card">
+      <img src=${image.webformatURL} alt=${image.tag} loading="lazy"/>
+      <div class="info">
+      <p class="info-item"><b>Likes: ${image.likes}</b></p>
+      <p class="info-item"><b>Views: ${image.views}</b></p>
+      <p class="info-item"><b>Comments: ${image.comments}</b></p>
+      <p class="info-item"><b>Downloads: ${image.downloads}</b></p>
+      </div>
+      </div>`;
     })
     .join('');
-  galleryOfImages.innerHTML = imagesMarkup;
+    galleryOfImages.insertAdjacentHTML('beforeend', imagesMarkup);
+}
+
+function checkLoadMoreBtnClass() {
+  if (loadMoreButton.classList.contains('is-hidden')) {
+    return;
+  }
+  loadMoreButton.classList.toggle('is-hidden');
+}
+
+async function onLoadMoreButtonClick() {
+    try {
+      const images = await fetchImagesFromPixabay.fetchImages();
+      renderImages(images.hits);
+    } catch (error) {
+      Notiflix.Notify.failure(`${error}`);
+      console.log(error);
+    }
+}
+
+
+
+
+async function onFormSubmit(e) {
+    e.preventDefault();
+    fetchImagesFromPixabay.page = 1;
+    galleryOfImages.innerHTML = '';
+    fetchImagesFromPixabay.searchQuery =
+      e.currentTarget.elements.searchQuery.value.trim();
+  try {
+    const images = await fetchImagesFromPixabay.fetchImages();
+    console.log(images);
+    if (images.total === 0) {
+      checkLoadMoreBtnClass();
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    renderImages(images.hits);
+    if (loadMoreButton.classList.contains('is-hidden')) {
+      loadMoreButton.classList.toggle('is-hidden');
+    }
+  } catch (error) {
+      Notiflix.Notify.failure(`${error}`);
+      console.log(error);
+  }
+  searchForm.reset();
 }
